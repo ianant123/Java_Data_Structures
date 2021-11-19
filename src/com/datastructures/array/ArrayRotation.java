454f6e2920011f19c4403e492eccb1de1ead90da
ianant123 <i.anant.123@gmail.com>
2021-11-19 14:39:26 +0530

    function rotate

---
+++ src/com/datastructures/array/ArrayRotation.java
@@ -0,0 +1,34 @@
+package com.datastructures.array;
+
+//https://www.geeksforgeeks.org/array-rotation/
+//Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
+
+public class ArrayRotation {
+
+    public static void leftRotate(int[] array, int d){
+        int length = array.length;
+        if(d <= 0){
+            return;
+        }
+        int i = 0;
+        int temp = array[i];
+        for(; i < length - 1; i++){
+            array[i] = array[i+1];
+        }
+        array[i] = temp;
+        leftRotate(array,d - 1);
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        leftRotate(arr, 2);
+        System.out.println("printing the array");
+        for (int j : arr) {
+            System.out.print(j + ",");
+        }
+    }
+}
